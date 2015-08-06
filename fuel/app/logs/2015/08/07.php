97755bc52fe78eeeb11824db68a06e47e773af8f
masa0226 <msk.u0226@gmail.com>
2015-08-07 01:36:06 +0900

    Delete 07.php

--- fuel/app/logs/2015/08/07.php
+++
@@ -1,9 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-ERROR - 2015-08-07 00:39:50 --> Error - Presenter "Presenter_Galleria_Hello" could not be found. in /Applications/XAMPP/xamppfiles/htdocs/galleria/fuel/core/classes/presenter.php on line 92
-ERROR - 2015-08-07 00:46:34 --> Error - Presenter "Presenter_Galleria_Hello" could not be found. in /Applications/XAMPP/xamppfiles/htdocs/galleria/fuel/core/classes/presenter.php on line 92
-ERROR - 2015-08-07 00:46:46 --> Error - File "APPPATH/classes/presenter/galleria/hello.php" does not contain class "Presenter_Galleria_Hello" in /Applications/XAMPP/xamppfiles/htdocs/galleria/fuel/core/classes/autoloader.php on line 397
-ERROR - 2015-08-07 00:48:01 --> Error - File "APPPATH/classes/presenter/galleria/404.php" does not contain class "Presenter_Galleria_404" in /Applications/XAMPP/xamppfiles/htdocs/galleria/fuel/core/classes/autoloader.php on line 397
-ERROR - 2015-08-07 00:48:53 --> Error - File "APPPATH/classes/presenter/galleria/404.php" does not contain class "Presenter_Galleria_404" in /Applications/XAMPP/xamppfiles/htdocs/galleria/fuel/core/classes/autoloader.php on line 397
-ERROR - 2015-08-07 00:49:11 --> Error - File "APPPATH/classes/presenter/galleria/404.php" does not contain class "Presenter_Galleria_404" in /Applications/XAMPP/xamppfiles/htdocs/galleria/fuel/core/classes/autoloader.php on line 397
-ERROR - 2015-08-07 01:12:51 --> Parsing Error - syntax error, unexpected '',' (T_CONSTANT_ENCAPSED_STRING), expecting ')' in /Applications/XAMPP/xamppfiles/htdocs/galleria/fuel/core/config/asset.php on line 87
